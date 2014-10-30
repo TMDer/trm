171b1e6379dd99184da402432bf503bd79078ed0
caesar_chi <caesar_chi@hiiir.com>
2014-10-30 18:32:37 +0800

    update make file

--- make.sh
+++ make.sh
@@ -4,5 +4,8 @@ npm install
 grunt compile
 
 browserify --entry out/trm.js --outfile lib/trm.compile.js
+browserify --entry out/trm.js --outfile static-server/lib/trm.compile.js
 npm run-script build
+cd ./static-server/
 noder --port 3000
+cd ..
