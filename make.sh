da20dad7d0f097be9964dfe9a2eb30ddeaca757a
clonn <clonncd@gmail.com>
2015-01-27 00:36:48 +0800

    update sh

--- make.sh
+++ make.sh
@@ -3,7 +3,6 @@
 npm install
 grunt compile
 
-browserify --entry out/trm.js --outfile lib/trm.compile.js
 browserify --entry out/trm.js --outfile static-server/lib/trm.compile.js
 npm run-script build
 cd ./static-server/
