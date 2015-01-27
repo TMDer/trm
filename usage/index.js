1c395a2e248843cd5f5a628e4432a5bf46b3fbd7
clonn <clonncd@gmail.com>
2015-01-27 20:02:28 +0800

    update include code

--- usage/index.js
+++ usage/index.js
@@ -1,17 +1,19 @@
 ;(function () {
-  window.analytics = {};
-  window.analytics.load = function (callback) {
-    if (!document.getElementById("analytics-js")) {
-        var a = document.createElement("script");
-        a.type = "text/javascript", a.id = "analytics-js", a.async = !0, a.src = ("https:" === document.location.protocol ? "https://" : "http://") + "{ENV_PATH}";
-        var n = document.getElementsByTagName("script")[0];
-        n.parentNode.insertBefore(a, n);
-    }
+  window.analytics = {
+    load: function (callback) {
+      if (!document.getElementById("analytics-js")) {
+          var a = document.createElement("script");
+          a.type = "text/javascript", a.id = "analytics-js", a.async = !0, a.src = ("https:" === document.location.protocol ? "https://" : "http://") + "{ENV_PATH}";
+          var n = document.getElementsByTagName("script")[0];
+          n.parentNode.insertBefore(a, n);
+      }
 
-    var event = window.onload;
-    window.onload = function () {
-      if (event) event();
-      return callback();
-    };
-  }, window.analytics.VERSION = "{VERSION}";
+      var event = window.onload;
+      window.onload = function () {
+        if (event) event();
+        return callback();
+      };  
+    },
+    VERSION: "{VERSION}"
+  };
 })();
