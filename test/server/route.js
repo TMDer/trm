feb82d48ce05deb544cb76bb6ec16f28f4edc371
clonn <clonncd@gmail.com>
2015-01-28 17:39:47 +0800

    update route and result for test

--- test/server/route.js
+++ test/server/route.js
@@ -1,27 +1,48 @@
 var serveStatic = require('serve-static');
 var serve = serveStatic(__dirname + '/static');
+var fs = require("fs");
+var path = require("path");
 var handle = require('finalhandler');
 var trm = require("../../out/import");
 
-var destPath = "./test/server/static/.tmp/test.js";
-trm.generateLib({
-  domain: "localhost:3000",
-  destPath: destPath,
-  minify: true
-});
 
-var optUrl = "localhost:3000/.tmp/test.js"
-trm.config(optUrl)
-var code = trm.compress().code + [
-  'window.analytics.load(function () {',
-  '  window.analytics.initial("-999", "-999");',
-  '  window.analytics.send("");',
-  '});'
-].join("");
-console.log(code)
 
-module.exports = function () {
+module.exports = function (port) {
+  var destPath = "./test/server/static/.tmp/test.js";
+
+  trm.generateLib({
+    domain: "localhost:" + port,
+    destPath: destPath,
+    minify: true
+  });
+
+  var optUrl = "localhost:" + port + "/.tmp/test.js";
+  trm.config(optUrl);
+
+  var code = trm.compress().code + [
+    'window.analytics.load(function () {',
+    '  window.analytics.initial("-999", "-999");',
+    '  window.analytics.send("");',
+    '});'
+  ].join("");
+
+  var code = trm.resultDisplay({
+    code: trm.compress().code,
+    pid: "-999",
+    aid: "-99999"
+  });
+
+  var indexRes = fs.readFileSync(path.join(__dirname, "/static/index.html"), "utf8")
+
   return function(req,res) {
+    if (req.url === "/track") {
+      return res.end("ok");
+    }
+
+    if (req.url === "/") {
+      return res.end(indexRes.replace("{{CODE}}", code));
+    }
+    
     var done = handle(req, res);
     serve(req, res, done);
   }
