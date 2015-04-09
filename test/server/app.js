174f0a0a4ea5ded4c33d1a294e1c95a2e8685840
clonn <clonncd@gmail.com>
2015-04-09 15:33:05 +0800

    fixed path and set port

--- test/server/app.js
+++ test/server/app.js
@@ -1,18 +1,21 @@
 var https = require("https");
 var http = require("http");
 var fs = require("fs");
+var path = require("path");
 var route = require("./route");
 
 var httpsPort = 8080;
 var httpPort = 3000;
 
+console.log(path.join(__dirname, '/keys/server.key'));
+
 var keys = {
-  key: fs.readFileSync('./keys/server.key'),
-  cert: fs.readFileSync('./keys/server.crt')
+  key: fs.readFileSync(path.join(__dirname, './keys/server.key')),
+  cert: fs.readFileSync(path.join(__dirname, './keys/server.crt'))
 };
 
-https.createServer(keys, route()).listen(httpsPort);
-http.createServer(route()).listen(httpPort);
+https.createServer(keys, route(httpsPort)).listen(httpsPort);
+http.createServer(route(httpPort)).listen(httpPort);
 
 console.log("create https server");
 console.log("https://localhost:" + httpsPort);
